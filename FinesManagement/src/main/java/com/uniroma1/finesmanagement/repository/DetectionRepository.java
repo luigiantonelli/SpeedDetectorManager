9627c9e22810fd190e51de8d12efbb15ce2b9eef
Lorenzo De Santis <desantis.1849114@studenti.uniroma1.it>
2022-08-20 15:36:56 +0200

    fixed project

--- FinesManagement/src/main/java/com/uniroma1/finesmanagement/repository/DetectionRepository.java
+++
@@ -1,8 +0,0 @@
-//package com.uniroma1.finesmanagement.repository;
-//
-//import entity.Detection;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//public interface DetectionRepository extends JpaRepository<Detection, Long> {
-//}
--- commons/src/main/java/com/uniroma1/commons/repository/SpeedCameraRepository.java
+++
@@ -1,7 +0,0 @@
-package com.uniroma1.commons.repository;
-
-import com.uniroma1.commons.entity.SpeedCamera;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SpeedCameraRepository extends JpaRepository<SpeedCamera, Long> {
-}
