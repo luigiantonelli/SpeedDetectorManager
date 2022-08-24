4b3f7bb918bd71cad5b67c1c85e1cbc0e2e53278
Lorenzo De Santis <desantis.1849114@studenti.uniroma1.it>
2022-08-24 13:57:23 +0200

    change PAI for Luca 2

--- PublicAdministrationInterface/src/main/java/com/example/application/data/service/AuthService.java
+++ PublicAdministrationInterface/src/main/java/com/example/application/data/service/AuthService.java
@@ -1,120 +1,120 @@
-//package com.example.application.data.service;
-//
-//import com.example.application.views.admin.AdminView;
-//import com.example.application.views.fines.FinesView;
-//import com.example.application.views.fines.GestiteView;
-//import com.example.application.views.fines.NuoveView;
-//import com.example.application.views.logout.LogoutView;
-//import com.example.application.views.main.MainView;
-//import com.vaadin.flow.component.Component;
-//import com.vaadin.flow.component.UI;
-//import com.vaadin.flow.router.RouteConfiguration;
-//import com.vaadin.flow.server.VaadinSession;
-//import it.uniroma1.commons.entity.User;
-//import it.uniroma1.commons.enums.Role;
-//import it.uniroma1.commons.repository.UserRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Optional;
-//
-//@Service
-//public class AuthService {
-//    @Autowired
-//    private UserRepository userRepository;
-//
-//    public record AuthorizedRoute(String route, String name, Class<? extends Component> view) {
-//
-//    }
-//
-//    public class AuthException extends Exception {
-//
-//    }
-//
-//    //private final UserRepository userRepository;
-//    // private final MailSender mailSender;
-//
-//    /*public AuthService(UserRepository userRepository, MailSender mailSender) {
-//        this.userRepository = userRepository;
-//        this.mailSender = mailSender;
-//    }*/
-//
-//    public void authenticate(String username, String password, String region) throws AuthException {
-//        //User user = userRepository.getByUsername(username);
-//        //prendi utente dal database
-//        Optional<User> optionalUser = userRepository.findById(username);
-//        User user = optionalUser.isPresent()?optionalUser.get():null;
-//        if (user != null && user.checkPassword(password) && user.checkRegion(region)/*user.isActive()*/) {
-//            VaadinSession.getCurrent().setAttribute(User.class, user);  // FORSE DA TOGLIERE
-//            createRoutes(user.getRole());
-//            if(user.getRole().equals(Role.ADMIN)){
-//                UI.getCurrent().navigate("admin");
-//            }
-//            else{
-//                UI.getCurrent().navigate("multe/nuove");
-//            }
-//        } else {
-//            throw new AuthException();
-//        }
-//    }
-//
-//    private void createRoutes(Role role) {
-//        getAuthorizedRoutes(role).stream()
-//                .forEach(route ->{
-//                        if( route.name.equals("Admin") || route.name.equals("Logout")){
-//                        RouteConfiguration.forSessionScope().setRoute(
-//                                route.route, route.view, MainView.class);}
-//                        else{
-//                            RouteConfiguration.forSessionScope().setRoute(
-//                                    route.route, route.view, FinesView.class);
-//                        }
-//                        }
-//                        );
-//
-//    }
-//
-//    public List<AuthorizedRoute> getAuthorizedRoutes(Role role) {
-//        ArrayList<AuthorizedRoute> routes = new ArrayList<>();
-//        routes.add(new AuthorizedRoute("logout", "Logout", LogoutView.class));
-//        if (role.equals(Role.USER)) {
-//            //routes.add(new AuthorizedRoute("login", "Login", LoginView.class));
-//            //routes.add(new AuthorizedRoute("multe", "Multe", FinesView.class));
-//            routes.add(new AuthorizedRoute("multe/nuove", "MulteNuove", NuoveView.class));
-//            routes.add(new AuthorizedRoute("multe/gestite", "MulteGestite", GestiteView.class));
-//
-//        }
-//        else if (role.equals(Role.ADMIN)) {
-//            //routes.add(new AuthorizedRoute("login", "Login", LoginView.class));
-//            //routes.add(new AuthorizedRoute("multe", "Multe", FinesView.class));
-//            /*routes.add(new AuthorizedRoute("multe", "Multe", FinesView.class));
-//            routes.add(new AuthorizedRoute("multe/nuove", "MulteNuove", NuoveView.class));
-//            routes.add(new AuthorizedRoute("multe/gestite", "MulteGestite", GestiteView.class));*/
-//            routes.add(new AuthorizedRoute("admin", "Admin", AdminView.class));
-//        }
-//        return routes;
-//    }
-//
-//   /* public void register(String email, String password) {
-//        User user = userRepository.save(new User(email, password, Role.USER));
-//        String text = "http://localhost:8080/activate?code=" + user.getActivationCode();
-//        SimpleMailMessage message = new SimpleMailMessage();
-//        message.setFrom("noreply@example.com");
-//        message.setSubject("Confirmation email");
-//        message.setText(text);
-//        message.setTo(email);
-//        mailSender.send(message);
-//    }*/
-//
-//    /*public void activate(String activationCode) throws AuthException {
-//        //User user = userRepository.getByActivationCode(activationCode)
-//        if (user != null) {
-//            user.setActive(true);
-//            userRepository.save(user);
-//        } else {
-//            throw new AuthException();
-//        }
-//    }*/
-//
-//}
+package com.example.application.data.service;
+
+import com.example.application.views.admin.AdminView;
+import com.example.application.views.fines.FinesView;
+import com.example.application.views.fines.GestiteView;
+import com.example.application.views.fines.NuoveView;
+import com.example.application.views.logout.LogoutView;
+import com.example.application.views.main.MainView;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.router.RouteConfiguration;
+import com.vaadin.flow.server.VaadinSession;
+import it.uniroma1.commons.entity.User;
+import it.uniroma1.commons.enums.Role;
+import it.uniroma1.commons.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AuthService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public record AuthorizedRoute(String route, String name, Class<? extends Component> view) {
+
+    }
+
+    public class AuthException extends Exception {
+
+    }
+
+    //private final UserRepository userRepository;
+    // private final MailSender mailSender;
+
+    /*public AuthService(UserRepository userRepository, MailSender mailSender) {
+        this.userRepository = userRepository;
+        this.mailSender = mailSender;
+    }*/
+
+    public void authenticate(String username, String password, String region) throws AuthException {
+        //User user = userRepository.getByUsername(username);
+        //prendi utente dal database
+        Optional<User> optionalUser = userRepository.findById(username);
+        User user = optionalUser.isPresent()?optionalUser.get():null;
+        if (user != null && user.checkPassword(password) && user.checkRegion(region)/*user.isActive()*/) {
+            VaadinSession.getCurrent().setAttribute(User.class, user);  // FORSE DA TOGLIERE
+            createRoutes(user.getRole());
+            if(user.getRole().equals(Role.ADMIN)){
+                UI.getCurrent().navigate("admin");
+            }
+            else{
+                UI.getCurrent().navigate("multe/nuove");
+            }
+        } else {
+            throw new AuthException();
+        }
+    }
+
+    private void createRoutes(Role role) {
+        getAuthorizedRoutes(role).stream()
+                .forEach(route ->{
+                        if( route.name.equals("Admin") || route.name.equals("Logout")){
+                        RouteConfiguration.forSessionScope().setRoute(
+                                route.route, route.view, MainView.class);}
+                        else{
+                            RouteConfiguration.forSessionScope().setRoute(
+                                    route.route, route.view, FinesView.class);
+                        }
+                        }
+                        );
+
+    }
+
+    public List<AuthorizedRoute> getAuthorizedRoutes(Role role) {
+        ArrayList<AuthorizedRoute> routes = new ArrayList<>();
+        routes.add(new AuthorizedRoute("logout", "Logout", LogoutView.class));
+        if (role.equals(Role.USER)) {
+            //routes.add(new AuthorizedRoute("login", "Login", LoginView.class));
+            //routes.add(new AuthorizedRoute("multe", "Multe", FinesView.class));
+            routes.add(new AuthorizedRoute("multe/nuove", "MulteNuove", NuoveView.class));
+            routes.add(new AuthorizedRoute("multe/gestite", "MulteGestite", GestiteView.class));
+
+        }
+        else if (role.equals(Role.ADMIN)) {
+            //routes.add(new AuthorizedRoute("login", "Login", LoginView.class));
+            //routes.add(new AuthorizedRoute("multe", "Multe", FinesView.class));
+            /*routes.add(new AuthorizedRoute("multe", "Multe", FinesView.class));
+            routes.add(new AuthorizedRoute("multe/nuove", "MulteNuove", NuoveView.class));
+            routes.add(new AuthorizedRoute("multe/gestite", "MulteGestite", GestiteView.class));*/
+            routes.add(new AuthorizedRoute("admin", "Admin", AdminView.class));
+        }
+        return routes;
+    }
+
+   /* public void register(String email, String password) {
+        User user = userRepository.save(new User(email, password, Role.USER));
+        String text = "http://localhost:8080/activate?code=" + user.getActivationCode();
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("noreply@example.com");
+        message.setSubject("Confirmation email");
+        message.setText(text);
+        message.setTo(email);
+        mailSender.send(message);
+    }*/
+
+    /*public void activate(String activationCode) throws AuthException {
+        //User user = userRepository.getByActivationCode(activationCode)
+        if (user != null) {
+            user.setActive(true);
+            userRepository.save(user);
+        } else {
+            throw new AuthException();
+        }
+    }*/
+
+}
--- PublicAdministrationInterface/src/main/java/com/example/application/views/admin/AdminView.java
+++ PublicAdministrationInterface/src/main/java/com/example/application/views/admin/AdminView.java
@@ -1,60 +1,60 @@
-//package com.example.application.views.admin;
-//
-//import com.example.application.data.service.AuthService;
-//import com.example.application.views.logout.LogoutView;
-//import com.vaadin.flow.component.button.Button;
-//import com.vaadin.flow.component.combobox.ComboBox;
-//import com.vaadin.flow.component.html.Div;
-//import com.vaadin.flow.component.html.H1;
-//import com.vaadin.flow.component.html.H2;
-//import com.vaadin.flow.component.orderedlayout.FlexComponent;
-//import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-//import com.vaadin.flow.component.textfield.PasswordField;
-//import com.vaadin.flow.component.textfield.TextField;
-//import com.vaadin.flow.router.PageTitle;
-//import com.vaadin.flow.router.RouterLink;
-//
-////@Route("admin") //NON ERA PRESENTE NEL PROGETTO ORIGINALE
-//
-//
-//@PageTitle("Admin | Pubblica amministrazione Repubblica Italiana")
-//public class AdminView extends Div {
-//
-//    public AdminView(AuthService auth){
-//        TextField user = new TextField("Username");
-//        PasswordField password1 = new PasswordField("Password");
-//        PasswordField password2 = new PasswordField("Conferma Password");
-//        ComboBox<String> select = new ComboBox<>();
-//        select.setPlaceholder("Seleziona regione");
-//
-//        select.setItems("Abruzzo", "Basilicata", "Calabria", "Campania",
-//                "Emilia Romagna", "Friuli-Venezia Giulia", "Lazio", "Liguria", "Lombardia", "Marche", "Molise", "Piemonte",
-//                "Puglia", "Sardegna", "Sicilia", "Toscana", "Trentino-Alto Adige", "Umbria", "Val d'Aosta", "Veneto");
-//
-//
-//        addClassName("admin-view");
-//        //setSizeFull();
-//
-//        VerticalLayout output = new VerticalLayout(
-//                new H1("Pubblica amministrazione Repubblica Italiana"),
-//                new H2("Registra nuovo utente"),
-//                user,
-//                password1,
-//                password2,
-//                select,
-//                new Button("Registra", event -> {
-//                    try {
-//
-//                        //controlla password1 == password2
-//                        //registra utente nel database con questi username, password1 e regione
-//                    } catch (Exception e) {
-//
-//                    }}),
-//                new RouterLink("Logout", LogoutView.class)
-//        );
-//        output.setAlignItems(FlexComponent.Alignment.CENTER);
-//        output.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
-//
-//        add(output);
-//    }
-//}
+package com.example.application.views.admin;
+
+import com.example.application.data.service.AuthService;
+import com.example.application.views.logout.LogoutView;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.RouterLink;
+
+//@Route("admin") //NON ERA PRESENTE NEL PROGETTO ORIGINALE
+
+
+@PageTitle("Admin | Pubblica amministrazione Repubblica Italiana")
+public class AdminView extends Div {
+
+    public AdminView(AuthService auth){
+        TextField user = new TextField("Username");
+        PasswordField password1 = new PasswordField("Password");
+        PasswordField password2 = new PasswordField("Conferma Password");
+        ComboBox<String> select = new ComboBox<>();
+        select.setPlaceholder("Seleziona regione");
+
+        select.setItems("Abruzzo", "Basilicata", "Calabria", "Campania",
+                "Emilia Romagna", "Friuli-Venezia Giulia", "Lazio", "Liguria", "Lombardia", "Marche", "Molise", "Piemonte",
+                "Puglia", "Sardegna", "Sicilia", "Toscana", "Trentino-Alto Adige", "Umbria", "Val d'Aosta", "Veneto");
+
+
+        addClassName("admin-view");
+        //setSizeFull();
+
+        VerticalLayout output = new VerticalLayout(
+                new H1("Pubblica amministrazione Repubblica Italiana"),
+                new H2("Registra nuovo utente"),
+                user,
+                password1,
+                password2,
+                select,
+                new Button("Registra", event -> {
+                    try {
+
+                        //controlla password1 == password2
+                        //registra utente nel database con questi username, password1 e regione
+                    } catch (Exception e) {
+
+                    }}),
+                new RouterLink("Logout", LogoutView.class)
+        );
+        output.setAlignItems(FlexComponent.Alignment.CENTER);
+        output.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+
+        add(output);
+    }
+}
